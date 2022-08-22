55ee23af98db8b6558cf51e3fb592c9b62bc8112
Aleksander Jaworski <aleck156@gmail.com>
2022-08-22 09:28:48 +0200

    refactor variable name

--- Day16/main.py
+++ Day16/main.py
@@ -3,14 +3,14 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 coffee_shop = CoffeeMaker()
-coffeee_menu = Menu()
+coffee_menu = Menu()
 money_account = MoneyMachine()
 
 
 make_coffee = True
 
 while make_coffee:
-    user_choice = input(f'What would you like to order? [{coffeee_menu.get_items()}report/quit]: ')[:1].lower()
+    user_choice = input(f'What would you like to order? [{coffee_menu.get_items()}report/quit]: ')[:1].lower()
     if user_choice == 'q':
         print('Thank you for visiting our coffee shop. See you next time!')
         make_coffee = False
@@ -18,7 +18,7 @@ while make_coffee:
         money_account.report()
         coffee_shop.report()
     else:
-        coffee_item = coffeee_menu.find_drink(user_choice)
+        coffee_item = coffee_menu.find_drink(user_choice)
         if coffee_item:
             if coffee_shop.is_resource_sufficient(coffee_item):
                 while not money_account.make_payment(coffee_item.cost):
