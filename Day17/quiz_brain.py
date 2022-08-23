99312a95507824ac85492bdf04b899e54c76ecf4
Aleksander Jaworski <aleck156@gmail.com>
2022-08-23 10:02:26 +0200

    completed quizz_game

---
+++ Day17/quiz_brain.py
@@ -0,0 +1,30 @@
+import random
+
+from question_model import Question
+
+class QuizzBrain:
+    def __init__(self, questions):
+        self.score = 0
+        self.question_bank = []
+        for q in questions:
+            self.question_bank.append(Question(q['text'], q['answer']))
+
+    def print_questions(self):
+        for index, question in enumerate(self.question_bank):
+            print(f'Q.{index}: {question.text}')
+            print(f'A.{index}: {question.answer}')
+
+    def pick_random_question(self):
+        print(f'Questions left in the bank: {len(self.question_bank)}')
+        if len(self.question_bank) > 0:
+            random_pick_number = random.randint(0, len(self.question_bank)-1)
+            print(f'Picking a random question ... {random_pick_number}')
+            question_picked = self.question_bank[random_pick_number]
+            self.question_bank.pop(random_pick_number)
+            return question_picked
+        else:
+            return False
+
+    def player_score(self):
+        return self.score
+
---
+++ Day17/quizz_game.py
@@ -0,0 +1,27 @@
+
+from data import question_data
+
+from quiz_brain import QuizzBrain
+
+brain = QuizzBrain(question_data)
+
+answer = True
+while answer:
+    question = brain.pick_random_question()
+    if question:
+        print(question.text)
+        user_choice = input(f'True or False? [true/false]: ').lower()[:1]
+        if user_choice == question.answer.lower()[:1]:
+            brain.score += 1
+            print(f'You current score is {brain.score}')
+        else:
+            answer = False
+    else:
+        print(f'Congratulations! You have answered all of the questions correctly.')
+        answer = False
+
+print(f'Your final score is {brain.score}')
+
+# TODO
+# remove the question from the list once it's been picked
+# the game runs till the user fails to give correct answer OR the game runs out of questions
