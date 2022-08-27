6e579a10fb6f3e1934b6780ccd9a37bc88b1fb97
Aleksander Jaworski <aleck156@gmail.com>
2022-08-27 14:21:54 +0200

    prepare the stage for the race

--- Day19/turtle_race.py
+++ Day19/turtle_race.py
@@ -2,13 +2,36 @@ from turtle import Turtle as t, Screen as s
 import random
 
 timmy = t()
-timmy.shape('turtle')
-
 sc = s()
-sc.colormode(255)
-sc.setup(800, 600)
+sc_w = 800
+sc_h = 600
+user_bet=1
+
+def random_color():
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+def init_game(turtle, screen, width, height):
+    turtle.shape('turtle')
+    user_pick = int(screen.numinput(title="Turtle Race",prompt='Which turtle will win? [1-6]: '))
+    screen.colormode(255)
+    screen.setup(width=width, height=height)
+    return user_pick
+
+def setup_turtles(width, height):
+    turtle_race = []
+    for index in range(0,6):
+        new_turtle = t()
+        new_turtle.shape('turtle')
+        new_turtle.color(random_color())
+        new_turtle.penup()
+        new_turtle.goto(- width /2 + 10 , -height / 4 + height * 0.1 * index)
+        new_turtle.pendown()
+        turtle_race.append(new_turtle)
+    return turtle_race
 
 
+user_bet = init_game(timmy, sc, sc_w, sc_h)
+setup_turtles(sc_w, sc_h)
 
 
 
