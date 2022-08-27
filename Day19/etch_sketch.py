9cc6fd4707757bf9b9d216100861e975206aea9a
Aleksander Jaworski <aleck156@gmail.com>
2022-08-27 09:14:02 +0200

    implement moving forward, backward and turning left and right

--- Day19/etch_sketch.py
+++ Day19/etch_sketch.py
@@ -7,6 +7,19 @@ screen = s()
 def move_forward(turtle):
     turtle.forward(100)
 
+def move_backward(turtle):
+    turtle.forward(-100)
+
+def turn_left(turtle):
+    turtle.left(90)
+
+def turn_right(turtle):
+    turtle.left(-90)
+
 screen.listen()
-screen.onkey(partial(move_forward, timmy), 'u')
+screen.onkey(partial(move_forward, timmy), 'Up')
+screen.onkey(partial(move_backward, timmy), 'Down')
+screen.onkey(partial(turn_left, timmy), 'Left')
+screen.onkey(partial(turn_right, timmy), 'Right')
+
 screen.exitonclick()
