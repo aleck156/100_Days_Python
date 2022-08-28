9a5b1266a0a51232c1903c13484323e671cfe5db
Aleksander Jaworski <aleck156@gmail.com>
2022-08-28 08:50:52 +0200

    bug fix - appending new default instance instead of properly set up one

--- Day20/snake_game.py
+++ Day20/snake_game.py
@@ -1,22 +1,34 @@
-from turtle import Turtle as t, Screen as s
+from turtle import Turtle, Screen
 import random
+from functools import partial
 
 score = 0
+screen = Screen()
 
 snake = []
-for index in range(0, 3):
-    new_snake_part = t()
-    new_snake_part.setx(index * (-25))
+for index in range(0, 5):
+    new_snake_part = Turtle(shape='square')
+    new_snake_part.penup()
+    new_snake_part.goto(index * (-20), 0)
     new_snake_part.color('white')
-    new_snake_part.shape('square')
-    snake.append(t())
+    snake.append(new_snake_part)
+print(snake)
 
-screen = s()
 screen.setup(width=1600, height=1200)
 screen.title('Welcome to the SNAKE GAME')
 screen.bgcolor('black')
 screen.colormode(255)
+
+def move_snake(snake):
+    for piece in snake:
+        piece.forward(20)
+
+for _ in range(0,10):
+    for seg in snake:
+        seg.forward(20)
+
 screen.listen()
+# screen.onkey(partial(move_snake,snake), 'space')
 
 screen.exitonclick()
 
