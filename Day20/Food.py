edc3556446e125e3dbb5f5d6933b6dd8dab9f046
Aleksander Jaworski <aleck156@gmail.com>
2022-08-30 16:48:50 +0200

    complete Food and Snake classes

--- Day20/Food.py
+++ Day20/Food.py
@@ -1,2 +1,19 @@
-class Food:
-    pass
+from turtle import Turtle
+import random
+# from snake_game import SCREEN_WIDTH, SCREEN_HEIGHT
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.penup()
+        # self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color('white')
+        self.speed('fastest')
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-800 + 25, 800 - 25)
+        random_y = random.randint(-600 + 25, 600 - 25)
+        self.goto(random_x, random_y)
--- Day20/snake_game.py
+++ Day20/snake_game.py
@@ -1,18 +1,22 @@
 import time
 from turtle import Turtle, Screen
-from Snake import Snake
-from Food import Food
-from Scoreboard import Scoreboard
 
-speed = 1
+from Food import Food
+from Snake import Snake
+
+
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 1200
+speed = 0.1
 screen = Screen()
 
 snake = Snake(3)
+food = Food()
 
 screen.tracer(0)
 # screen.update()
 
-screen.setup(width=1600, height=1200)
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.title('Welcome to the SNAKE GAME')
 screen.bgcolor('black')
 screen.colormode(255)
@@ -28,7 +32,9 @@ screen.onkey(snake.right, "Right")
 while True:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    if snake.head.distance(food) < 25:
+        food.refresh()
 
 screen.exitonclick()
