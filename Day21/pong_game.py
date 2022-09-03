5746fb688a191d2608f3d70e0a7611cbf6de4985
Aleksander Jaworski <aleck156@gmail.com>
2022-09-03 09:46:22 +0200

    initial screen set up complete

--- Day21/pong_game.py
+++ Day21/pong_game.py
@@ -1,10 +1,26 @@
+import time
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 1200
 
 screen = Screen()
 screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.bgcolor('black')
+screen.title('Ping-Pong game')
+
+paddle = Paddle()
+
+screen.tracer(0)
 
 screen.listen()
-screen.onkey(screen.exitonclick, 'space')
+screen.onkey(screen.bye, 'space')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+
+# screen.onkey(screen.exitonclick, 'space')
