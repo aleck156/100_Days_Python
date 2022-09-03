1bbb971713d55ff50a72574961a632fa2bba9dea
Aleksander Jaworski <aleck156@gmail.com>
2022-09-03 08:47:43 +0200

    code refactor - collision detection

--- Day20/snake_game.py
+++ Day20/snake_game.py
@@ -46,9 +46,7 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-    for segment in snake.snake:
-        if segment == snake.head:
-            pass
+    for segment in snake.snake[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
