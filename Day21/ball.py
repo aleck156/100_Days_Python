4e8ec0f57318243e814e791d683c1edbfa09d2eb
Aleksander Jaworski <aleck156@gmail.com>
2022-09-04 08:46:24 +0200

    add ball bouncing off upper and lower walls and both paddles

--- Day21/ball.py
+++ Day21/ball.py
@@ -7,8 +7,16 @@ class Ball(Turtle):
         self.shape('circle')
         self.color('red')
         self.penup()
+        self.x_move = 0.1
+        self.y_move = 0.1
 
     def move(self):
-        new_x = self.xcor() + 0.1
-        new_y = self.ycor() + 0.1
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- Day21/pong_game.py
+++ Day21/pong_game.py
@@ -3,32 +3,41 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 
-SCREEN_WIDTH = 1600
-SCREEN_HEIGHT = 1200
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 screen = Screen()
 screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.bgcolor('black')
 screen.title('Ping-Pong game')
 
-l_paddle = Paddle(-SCREEN_WIDTH/2 + 40 , 40)
-r_paddle = Paddle(SCREEN_WIDTH/2 - 40 , 40)
+l_paddle = Paddle(-SCREEN_WIDTH/2 + 40, 40)
+r_paddle = Paddle(SCREEN_WIDTH/2 - 40, 40)
 ball = Ball()
 
 screen.tracer(0)
 
 screen.listen()
 screen.onkey(screen.bye, 'space')
-screen.onkey(l_paddle.move_up, 'Up')
-screen.onkey(l_paddle.move_down, 'Down')
-screen.onkey(r_paddle.move_up, 'w')
-screen.onkey(r_paddle.move_down, 's')
+screen.onkey(l_paddle.move_up, 'w')
+screen.onkey(l_paddle.move_down, 's')
+screen.onkey(r_paddle.move_up, 'Up')
+screen.onkey(r_paddle.move_down, 'Down')
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.001)
     screen.update()
     ball.move()
+
+    if ball.ycor() > SCREEN_HEIGHT / 2 - 20 or ball.ycor() < -SCREEN_HEIGHT / 2 + 20:
+        ball.bounce_y()
+
+    if ball.xcor() > SCREEN_HEIGHT / 2 - 50 and ball.distance(r_paddle.pos()) < 40:
+        ball.bounce_x()
+
+    if ball.xcor() < -SCREEN_HEIGHT / 2 + 50 and ball.distance(l_paddle.pos()) < 40:
+        ball.bounce_x()
 
 
 # screen.onkey(screen.exitonclick, 'space')
