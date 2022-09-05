02f7895395514a14edb4949db7349275b3d52440
Aleksander Jaworski <aleck156@gmail.com>
2022-09-05 09:59:31 +0200

    automate generating letters

--- Day24/Mail Merge Project Start/main.py
+++ Day24/Mail Merge Project Start/main.py
@@ -6,3 +6,16 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open('./Input/Letters/starting_letter.txt') as starting_letter:
+    letter_template = starting_letter.read()
+
+# print(letter_template)
+
+with open('./Input/Names/invited_names.txt') as names:
+    names = names.read().split('\n')
+    for name in names:
+        new_letter = letter_template.replace('[name]', name)
+        # save the new template
+        with open(f'./Output/ReadyToSend/{name}', mode='w') as output_file:
+            output_file.write(new_letter)
