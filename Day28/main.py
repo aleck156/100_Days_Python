7d10498887d7d5540100b1a3e78dc9b51e8a9ad7
Aleksander Jaworski <aleck156@gmail.com>
2022-09-09 08:56:44 +0200

    implement base layout of components

--- Day28/main.py
+++ Day28/main.py
@@ -12,6 +12,9 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+TICK_SYMBOL = '✔'
+TICK_COUNT = 7
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -22,26 +25,35 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title('Pomodoro App!')
-window.config(padx=10, pady=10, bg=PINK)
+window.config(padx=10, pady=10, bg=YELLOW)
 
 canvas = Canvas(width=250, height=250, bg=YELLOW, highlightthickness=0)
 image = PhotoImage(file='./tomato.png')
 canvas.create_image(125, 125, image=image)
-canvas.create_text(125, 125, text='25:00', font=(FONT_NAME, 20, 'bold'), fill='white')
-canvas.grid(column=0, row=0, columnspan=2)
+canvas.create_text(125, 150, text='25:00', font=(FONT_NAME, 20, 'bold'), fill='white')
+canvas.grid(column=0, row=1, columnspan=3)
 
-# create 1 text display
+# create 1 text display on top
+top_label = Label()
+top_label.config(text='TIMER')
+top_label.grid(column=0, row=0, columnspan=3)
 
 
 # create 2 buttons - start, stop
 # add 2 methods
 button_left = Button()
 button_left.config(text='Start')
-button_left.grid(column=0, row=1)
+button_left.grid(column=0, row=2)
 
 button_right = Button()
 button_right.config(text='Stop')
-button_right.grid(column=1, row=1)
+button_right.grid(column=2, row=2)
+
+# bottom tick
+text = Label()
+current_ticks = ''.join([TICK_SYMBOL for _ in range(0, TICK_COUNT)])
+text.config(text=current_ticks)
+text.grid(column=1, row=3)
 
 
 # window.minsize(width=400, height=600)
