2f5a8ae59ac714ff118cdeecb7a7c1d24a70d0f5
Aleksander Jaworski <aleck156@gmail.com>
2022-09-09 08:40:59 +0200

    insert background picture, add a text on top of it, attach 2 buttons below the image

--- Day28/main.py
+++ Day28/main.py
@@ -22,9 +22,30 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title('Pomodoro App!')
-window.minsize(width=400, height=600)
-window.config(padx=10, pady=10)
-window.grid()
+window.config(padx=10, pady=10, bg=PINK)
+
+canvas = Canvas(width=250, height=250, bg=YELLOW, highlightthickness=0)
+image = PhotoImage(file='./tomato.png')
+canvas.create_image(125, 125, image=image)
+canvas.create_text(125, 125, text='25:00', font=(FONT_NAME, 20, 'bold'), fill='white')
+canvas.grid(column=0, row=0, columnspan=2)
+
+# create 1 text display
+
+
+# create 2 buttons - start, stop
+# add 2 methods
+button_left = Button()
+button_left.config(text='Start')
+button_left.grid(column=0, row=1)
+
+button_right = Button()
+button_right.config(text='Stop')
+button_right.grid(column=1, row=1)
+
+
+# window.minsize(width=400, height=600)
+# window.grid()
 
 
 window.mainloop()
