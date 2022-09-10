717b2ec25f7dc149aab33bd0b2076c6394d52487
Aleksander Jaworski <aleck156@gmail.com>
2022-09-10 09:48:27 +0200

    display logo on canvas

--- Day29/main.py
+++ Day29/main.py
@@ -1,5 +1,20 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Password Manager')
+window.config(padx=20, pady=20)
+window.minsize(width=200, height=200)
+
+canvas = Canvas(width=200, height=200, bg='white')
+image = PhotoImage(file='./logo.png')
+canvas.create_image(100, 100, image=image)
+canvas.grid(column=0, row=0)
+
+
+
+window.mainloop()
