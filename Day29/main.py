bb87359c1a415d1b696f2749228ad8211ee10e77
Aleksander Jaworski <aleck156@gmail.com>
2022-09-11 14:53:40 +0200

    update how final password is concatenated

--- Day29/main.py
+++ Day29/main.py
@@ -4,7 +4,7 @@ from random import choice, randint, shuffle
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
-def generate_password()
+def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -16,9 +16,7 @@ def generate_password()
 
     shuffle(password_list)
 
-    password = ""
-    for char in password_list:
-      password += char
+    password = "".join(password_list)
 
     print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
