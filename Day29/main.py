e8c724d811bba48812ef42ee2b03406926a2a317
Aleksander Jaworski <aleck156@gmail.com>
2022-09-11 15:00:58 +0200

    rename password generating method, attach it to the button, insert generated value into Entry

--- Day29/main.py
+++ Day29/main.py
@@ -4,7 +4,7 @@ from random import choice, randint, shuffle
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
-def generate_password():
+def create_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -18,7 +18,10 @@ def generate_password():
 
     password = "".join(password_list)
 
-    print(f"Your password is: {password}")
+    print(f"Your new password is: {password}")
+    if password_value.get():
+        password_value.delete(0, 'end')
+    password_value.insert(0, password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_data():
@@ -86,7 +89,7 @@ password_value.config()
 password_value.grid(column=1, row=3, sticky='w')
 
 generate_password = Button(width=20)
-generate_password.config(text='Generate Password')
+generate_password.config(text='Generate Password', command=create_password)
 generate_password.grid(column=2, row=3)
 # ROW 4
 add_button = Button(width=52)
