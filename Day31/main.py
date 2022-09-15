0d1e8c6db761863365ff5bc7b7a069de070bacb5
Aleksander Jaworski <aleck156@gmail.com>
2022-09-15 08:05:35 +0200

    insert flashcard image as canvas

--- Day31/main.py
+++ Day31/main.py
@@ -8,6 +8,15 @@ POSITIVE = "✅"
 window = Tk()
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 window.minsize(height=400, width=600)
+window.title('FlashCard')
+
+# use canvas to layer things on top of each other
+canvas = Canvas(width=800, height=526)
+card_front_image = PhotoImage(file='./images/card_front.png')
+card_back_image = PhotoImage(file='./images/card_back.png')
+canvas.create_image(400,263, image=card_front_image)
+canvas.grid(row=0, column=0, columnspan=2)
+
 
 # ROW 1
 
