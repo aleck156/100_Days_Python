8d72e702f4980813eb491ea77ce35b3ac05f9979
Aleksander Jaworski <aleck156@gmail.com>
2022-09-18 14:13:44 +0200

    read a sample letter, send a sample email

--- Day32/main.py
+++ Day32/main.py
@@ -46,6 +46,7 @@ import random
 
 MY_EMAIL = 'gmail@test.com'
 PASS = 'TSET321'
+HOST = 'smtp.gmail.com'
 
 today_tuple = (dt.datetime.now().month,dt.datetime.now().day)
 birthdays = pd.read_csv('./birthdays.csv')
@@ -53,7 +54,20 @@ birthdays = pd.read_csv('./birthdays.csv')
 birthdays_dict = {(data_row['month'], data_row['day']):data_row for (index, data_row) in birthdays.iterrows()}
 
 if today_tuple in birthdays_dict:
-    # file_path =
+    file_path = f'./letter_templates/letter_{random.randint(1,3)}.txt'
+    birthday_person = birthdays_dict[today_tuple]
+    with open(file_path) as file:
+        new_text = file.read().replace('[NAME]', birthday_person['name'])
+
+    with smtplib.SMTP(HOST) as connection:
+        connection.starttls()
+        connection.login()
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person['email'],
+            msg=new_text
+        )
+
 
 with open('./quotes.txt', mode='r') as quotes_file:
     quotes = quotes_file.read().split('\n')
