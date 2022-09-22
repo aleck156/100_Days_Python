ae639ce272daf56f46f6c4a8a16bf55f2bf2e888
Aleksander Jaworski <aleck156@gmail.com>
2022-09-22 18:01:08 +0200

    define base canvas props

--- Day34/ui.py
+++ Day34/ui.py
@@ -14,8 +14,11 @@ class QuizzInterface:
         # row 1
         self.score_label = tkinter.Label(text=f'Score: {0}', fg='white', bg=THEME_COLOR)
         self.score_label.grid(column=1, row=0)
-        # row 2
 
+        # row 2
+        self.canvas = tkinter.Canvas(width=300, height=250, bg='white')
+
+        self.canvas.grid(row=1, column=0, columnspan=2)
         # row 3
 
         self.window.mainloop()
