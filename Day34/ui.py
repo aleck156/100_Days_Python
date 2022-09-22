1087442fd84d46d4eb97cf1fa8c726d637383417
Aleksander Jaworski <aleck156@gmail.com>
2022-09-22 17:57:37 +0200

    define first row of ui

--- Day34/ui.py
+++ Day34/ui.py
@@ -1,10 +1,21 @@
+import tkinter
 from tkinter import *
 
 
 THEME_COLOR = "#375362"
+FONT_CONFIG = ('Arial', 20, 'italix')
 
 class QuizzInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(pady=20, padx=20, bg=THEME_COLOR)
+
+        # row 1
+        self.score_label = tkinter.Label(text=f'Score: {0}', fg='white')
+        self.score_label.grid(column=1, row=0)
+        # row 2
+
+        # row 3
+
         self.window.mainloop()
