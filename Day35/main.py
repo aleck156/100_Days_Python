008581298bd33a099a6bdc469d8e83818c0c0a6e
Aleksander Jaworski <aleck156@gmail.com>
2022-09-25 14:59:33 +0200

    changeAPI endpoint to 2.5/weather

--- Day35/main.py
+++ Day35/main.py
@@ -1,4 +1,4 @@
-# from Personal_Projects/API_KEYS.py import OPENWEATHERMAP_API_KEY as API_key
+import requests
 from Personal_Projects import API_KEYS
 
 lat = 50.064651
@@ -6,6 +6,10 @@ lon = 19.944981
 
 API_key = API_KEYS.OPENWEATHERMAP_API_KEY
 
-URL = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_key}'
+URL = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}'
+
+res = requests.get(URL)
 
 print(URL)
+print(res)
+print(res.json())
