ac2d2ef7a713c74abfb7482f8e1034f08921fa27
Aleksander Jaworski <aleck156@gmail.com>
2022-09-25 14:46:23 +0200

    import api key from a separate file

--- Day35/main.py
+++ Day35/main.py
@@ -1,4 +1,11 @@
+# from Personal_Projects/API_KEYS.py import OPENWEATHERMAP_API_KEY as API_key
+from Personal_Projects import API_KEYS
+
 lat = 50.064651
 lon = 19.944981
 
-URL = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+API_key = API_KEYS.OPENWEATHERMAP_API_KEY
+
+URL = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_key}'
+
+print(URL)
