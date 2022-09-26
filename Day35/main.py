ef8eeac91c5842d7221cf6032ca472cb09bd00af
Aleksander Jaworski <aleck156@gmail.com>
2022-09-26 19:00:31 +0200

    fetch the data, parse it to separate elements

--- Day35/main.py
+++ Day35/main.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 from Personal_Projects import API_KEYS
 
@@ -6,17 +8,42 @@ lon = 19.944981
 
 API_key = API_KEYS.OPENWEATHERMAP_API_KEY
 
-OWN_Endpoint = 'https://api.openweathermap.org/data/2.5/forecast'
+# OWN_Endpoint = 'https://api.openweathermap.org/data/2.5/forecast'
+# weather_params = {
+#     'lat': lat,
+#     'lon': lon,
+#     'appid': API_key,
+#     'exclude': 'current,minutely,daily'
+# }
+#
+# res = requests.get(OWN_Endpoint, params=weather_params)
+# res.raise_for_status()
+#
+# weather_data = res.json()
+# weather_slice = weather_data["hourly"][:12]
+# for hour_data in weather_slice:
+#     print(hour_data['weather'])
+#
+# print(API_key)
+# print(res)
+# print(res.json())
+
+# http://api.openweathermap.org/data/2.5/forecast?lat=50.064651&lon=19.944981&appid=
+OWM_Endpoint = f'http://api.openweathermap.org/data/2.5/forecast'
 weather_params = {
     'lat': lat,
     'lon': lon,
     'appid': API_key
 }
 
-res = requests.get(OWN_Endpoint, params=weather_params)
+res = requests.get(OWM_Endpoint, params=weather_params)
 res.raise_for_status()
+data_set = res.json()['list']
 
-print(API_key)
-print(res)
-print(res.json())
-
+for data_piece in data_set:
+    # print(data_piece)
+    extract_time =datetime.datetime.fromtimestamp(data_piece['dt'])
+    main = data_piece['weather'][0]['main']
+    description = data_piece['weather'][0]['description']
+    condition_code = data_piece['weather'][0]['id']
+    print(f'[{extract_time}]:\t{condition_code}\t{main}\t{description}')
