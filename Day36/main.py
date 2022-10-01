212d128d3210daf08743338c6399fff46b75b380
Aleksander Jaworski <aleck156@gmail.com>
2022-10-01 10:38:37 +0200

    calculate the difference in prices

--- Day36/main.py
+++ Day36/main.py
@@ -14,5 +14,18 @@ stock_params = {
 }
 res = requests.get(STOCK_ENDPOINT, params=stock_params)
 res.raise_for_status()
+data = res.json()['Time Series (Daily)']
+data_list = [value for (key, value) in data.items()]
+yesterday = data_list[0]
 
-print(res.json())
+# data points
+yesterday_closing_price = float(yesterday['4. close'])
+day_before_yesterday_closing_price = float(data_list[1]['4. close'])
+
+difference = yesterday_closing_price - day_before_yesterday_closing_price
+
+diff_percent = (difference / yesterday_closing_price) * 100
+print(diff_percent)
+
+if diff_percent > 5:
+    print('get news')
