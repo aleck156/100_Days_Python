cef84279111e28fd6ada83d6f8d303d494614c31
Aleksander Jaworski <aleck156@gmail.com>
2022-10-01 08:59:17 +0200

    import api keys

---
+++ Day36/main.py
@@ -0,0 +1,6 @@
+from Personal_Projects import API_KEYS as ak
+STOCK_NAME = "TSLA"
+COMPANY_NAME = 'Tesla, inc.'
+
+STOCK_ENDPOINT = 'https://www.alphavantage.co/query'
+NEWS_ENDPOINT = 'https://newsapi.org/v2/everything'
