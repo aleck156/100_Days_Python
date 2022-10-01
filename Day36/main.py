07c7e38d0676dac8b79740a2d1b6e66d86f569eb
Aleksander Jaworski <aleck156@gmail.com>
2022-10-01 12:20:41 +0200

    fetch the news about the company

--- Day36/main.py
+++ Day36/main.py
@@ -2,23 +2,30 @@ import requests
 
 from Personal_Projects import API_KEYS as ak
 STOCK_NAME = "TSLA"
-COMPANY_NAME = 'Tesla, inc.'
+COMPANY_NAME = 'Tesla'
 
 STOCK_ENDPOINT = 'https://www.alphavantage.co/query'
 NEWS_ENDPOINT = 'https://newsapi.org/v2/everything'
 
+# PARAMS
 stock_params = {
     'function': 'TIME_SERIES_DAILY',
     'symbol': STOCK_NAME,
     'apikey': ak.STOCK_API_KEY
 }
-res = requests.get(STOCK_ENDPOINT, params=stock_params)
-res.raise_for_status()
-data = res.json()['Time Series (Daily)']
+
+news_param = {
+    'apiKey': ak.NEWS_API,
+    'qInTitle': COMPANY_NAME
+}
+
+# STOCK
+stock_response = requests.get(STOCK_ENDPOINT, params=stock_params)
+stock_response.raise_for_status()
+data = stock_response.json()['Time Series (Daily)']
 data_list = [value for (key, value) in data.items()]
 yesterday = data_list[0]
 
-# data points
 yesterday_closing_price = float(yesterday['4. close'])
 day_before_yesterday_closing_price = float(data_list[1]['4. close'])
 
@@ -28,4 +35,6 @@ diff_percent = (difference / yesterday_closing_price) * 100
 print(diff_percent)
 
 if diff_percent > 5:
-    print('get news')
+    news_response = requests.get(NEWS_ENDPOINT, params=news_param)
+    news_response.raise_for_status()
+    print(news_response.json()['articles'])
