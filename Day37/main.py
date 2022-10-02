d04f26da75cee2e37e8af933f779abe404840774
Aleksander Jaworski <aleck156@gmail.com>
2022-10-02 14:47:56 +0200

    import datetime, use it

--- Day37/main.py
+++ Day37/main.py
@@ -1,5 +1,6 @@
 import requests
 from Personal_Projects import API_KEYS as ak
+from datetime import datetime as dt
 
 pixela_endpoint = 'https://pixe.la/v1/users/'
 
@@ -20,12 +21,12 @@ endpoint_params = {
 
 
 
-graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
+graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/'
 graphID = 'graph1'
 
 graph_config = {
     'id': graphID,
-    'name': 'cycling graph',
+    'name': 'cycling_graph',
     'unit': 'km',
     'type': 'float',
     'color': 'ajisai'
@@ -35,9 +36,23 @@ pixela_headers = {
     'X-USER-TOKEN': ak.PIXELA_TOKEN
 }
 
-# response_graph = requests.post(url=graph_endpoint, json=graph_config, headers=pixela_headers)
+response_graph = requests.post(url=graph_endpoint, json=graph_config, headers=pixela_headers)
+print(response_graph.text)
 # with open('./response_txt.html', mode='w+') as file:
 #     file.write(response_graph.text)
-# print(response_graph.text)
 
-pixel_creation = f'{pixela_endpoint}{USERNAME}/graphs/{graphID}'
+pixel_creation = f'{pixela_endpoint}/{USERNAME}/graphs/{graphID}'
+
+print(pixel_creation)
+
+pixel_data = {
+    'date':         dt.now().strftime('%Y%m%d'),
+    'quantity':     '9.74',
+}
+
+
+response_pixel_creation = requests.post(url=pixel_creation, json=pixel_data, headers=pixela_headers)
+print(response_pixel_creation.text)
+
+
+# f'{dt.now().year}{dt.now().month}{dt.now().day:02d}'
