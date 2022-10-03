70c58a660f84ae66b090648111162cdebb47973f
Aleksander Jaworski <aleck156@gmail.com>
2022-10-03 09:04:29 +0200

    complete the automation

--- Day38/main.py
+++ Day38/main.py
@@ -1,4 +1,5 @@
 import requests
+import datetime as dt
 from Personal_Projects import API_KEYS as ak
 
 
@@ -26,4 +27,36 @@ message = {
 
 response = requests.post(url=f'{SITE_ENDPOINT}{EXERCISE_ENDPOINT}', headers=app_headers, json=message)
 response.raise_for_status()
-print(response.json())
+data = response.json()['exercises'][0]
+print(data)
+
+current_date = dt.datetime.now().strftime('%d/%m/%Y')
+exercise_type = data['name']
+exercise_duration = data['duration_min']
+calories_burned = data['nf_calories']
+
+# SHEETLY
+SHEETLY_ENDPOINT = 'https://api.sheety.co/'
+SHEETLY_USERNAME = ''
+SHEETLY_GET = ak.SHEETLY_GET
+SHEETLY_POST =  ak.SHEETLY_POST
+workouts = {
+    'workouts': {
+        'date': current_date,
+        'time': exercise_duration,
+        'exercises': exercise_type,
+        'duration': exercise_duration,
+        'calories': calories_burned
+    }
+}
+
+
+sheetly_post_res = requests.post(url=SHEETLY_POST, json=workouts)
+sheetly_post_res.raise_for_status()
+print(sheetly_post_res)
+# print(workouts)
+
+# sheetly_res = requests.get(url=SHEETLY_GET)
+# print(sheetly_res.text)
+
+# Date	Time	Exercise	Duration	Calories
