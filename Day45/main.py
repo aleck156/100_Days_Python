8cf834b0d2ccb315ddb9cbd8b3f29a4cae747b9e
Aleksander Jaworski <aleck156@gmail.com>
2022-10-28 14:52:59 +0200

    start scraping y combinator

--- Day45/main.py
+++ Day45/main.py
@@ -1,1 +1,20 @@
 from bs4 import BeautifulSoup
+
+with open('./website.html', mode='r') as file:
+    content = file.read()
+
+soup = BeautifulSoup(content, 'html.parser')
+# print(soup.title.string)
+
+# print(soup.find_all(name='a')[:])
+
+anchor_tags = soup.find_all(name='a')
+
+for tag in anchor_tags:
+    print(tag['href'])
+    print(tag.getText())
+
+heading = soup.find_all(name='h1')
+title_heading = heading['id' == 'name']
+print(title_heading.string)
+
