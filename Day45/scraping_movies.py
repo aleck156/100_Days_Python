6b34a0bc949e56204485ef322c978220489b3120
Aleksander Jaworski <aleck156@gmail.com>
2022-10-31 08:49:46 +0100

    complete web scraping project

--- Day45/scraping_movies.py
+++ Day45/scraping_movies.py
@@ -5,5 +5,19 @@ import requests
 URL = 'https://www.empireonline.com/movies/features/best-movies-2/'
 
 res = requests.get(URL)
+empireonline_website = res.text
 
-print(res.text)
+EO_soup = BeautifulSoup(empireonline_website, 'html.parser')
+
+movies_list = []
+
+elements = EO_soup.find_all(name='picture')
+for elem in elements:
+    elem_img = elem.find_all(name='img')
+    movies_list.append(elem_img[0]['alt'])
+
+print(movies_list)
+
+with open('movies_list.txt', mode='w+') as file:
+    for movie in movies_list[::-1]:
+        file.write(f'{movie}\n')
