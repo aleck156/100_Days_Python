e511f5bfc241842934cba44fdf0ba9c9baad1cbe
Aleksander Jaworski <aleck156@gmail.com>
2022-10-31 09:04:20 +0100

    set up the baseline of the project

---
+++ Day46/main.py
@@ -0,0 +1,6 @@
+# find the top 100 music that was all the hot stuff back in the author's teenage years
+# get users birthyear
+# add 15
+# URL = f'https://billboard.com/charts/hot-100/{year}-{month}-{day}'
+# fetch 100 songs off Billboard rankings from that year
+
