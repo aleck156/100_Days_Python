479ff35cc58d7715abb4d2822734ebb11e2c6c0c
Aleksander Jaworski <aleck156@gmail.com>
2022-11-01 09:19:22 +0100

    automate filling out online forms using selenium web driver

--- Day48/interactions.py
+++ Day48/interactions.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.common.keys import Keys
+
 URL = 'https://en.wikipedia.org/wiki/Main_Page'
 
 chrome_driver_path = "/home/railgun/Downloads/chromedriver_linux64/chromedriver"
 driver = webdriver.Chrome(chrome_driver_path)
 
 driver.get(URL)
-find_number = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
+article_count = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
+print(article_count.text)
+article_count.click()
 
-print(find_number.text)
+search_bar = driver.find_element(By.NAME, 'search')
+search_bar.send_keys("Python")
+search_bar.send_keys(Keys.ENTER)
 
-driver.quit()
+# driver.quit()
